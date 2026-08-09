d8d3bccf45b8b5c15e23de3eb33bdaea938bc49b
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-08-08 23:34:40 -0400

    mejoras del menu de inicio y añadido de assets

--- js/game.js
+++ js/game.js
@@ -58,29 +58,29 @@ class Cards {
     }
 }
 
-let bone = new Cards('bone', '/assets/bone.png', 5, 'organ', 'yellow');
-let brain = new Cards('brain', '/assets/brain.png', 5, 'organ', 'blue');
-let heart = new Cards('heart', '/assets/heart.png', 5, 'organ', 'red');
-let stomach = new Cards('stomach', '/assets/stomach.png', 5, 'organ', 'green');
-let nervousSystem = new Cards('nervousSystem', '/assets/rainbow_organ.png', 1, 'organ', 'rainbow');
+let bone = new Cards('bone', '/assets/cards/bone.png', 5, 'organ', 'yellow');
+let brain = new Cards('brain', '/assets/cards/brain.png', 5, 'organ', 'blue');
+let heart = new Cards('heart', '/assets/cards/heart.png', 5, 'organ', 'red');
+let stomach = new Cards('stomach', '/assets/cards/stomach.png', 5, 'organ', 'green');
+let nervousSystem = new Cards('nervousSystem', '/assets/cards/rainbow_organ.png', 1, 'organ', 'rainbow');
 
-let yellowMedicine = new Cards('yellow_medicine', '/assets/yellow_medicine.png', 4, 'medicine', 'yellow');
-let blueMedicine = new Cards('blue_medicine', '/assets/blue_medicine.png', 4, 'medicine', 'blue');
-let redMedicine = new Cards('red_medicine', '/assets/red_medicine.png', 4, 'medicine', 'red');
-let greenMedicine = new Cards('green_medicine', '/assets/green_medicine.png', 4, 'medicine', 'green');
-let rainbowMedicine = new Cards('rainbow_medicine', '/assets/rainbow_medicine.png', 4, 'medicine', 'rainbow');
+let yellowMedicine = new Cards('yellow_medicine', '/assets/cards/yellow_medicine.png', 4, 'medicine', 'yellow');
+let blueMedicine = new Cards('blue_medicine', '/assets/cards/blue_medicine.png', 4, 'medicine', 'blue');
+let redMedicine = new Cards('red_medicine', '/assets/cards/red_medicine.png', 4, 'medicine', 'red');
+let greenMedicine = new Cards('green_medicine', '/assets/cards/green_medicine.png', 4, 'medicine', 'green');
+let rainbowMedicine = new Cards('rainbow_medicine', '/assets/cards/rainbow_medicine.png', 4, 'medicine', 'rainbow');
 
-let yellowVirus = new Cards('yellow_virus', '/assets/yellow_virus.png', 4, 'virus', 'yellow');
-let blueVirus = new Cards('blue_virus', '/assets/blue_virus.png', 4, 'virus', 'blue');
-let redVirus = new Cards('red_virus', '/assets/red_virus.png', 4, 'virus', 'red');
-let greenVirus = new Cards('green_virus', '/assets/green_virus.png', 4, 'virus', 'green');
-let rainbowVirus = new Cards('rainbow_virus', '/assets/rainbow_virus.png', 1, 'virus', 'rainbow');
+let yellowVirus = new Cards('yellow_virus', '/assets/cards/yellow_virus.png', 4, 'virus', 'yellow');
+let blueVirus = new Cards('blue_virus', '/assets/cards/blue_virus.png', 4, 'virus', 'blue');
+let redVirus = new Cards('red_virus', '/assets/cards/red_virus.png', 4, 'virus', 'red');
+let greenVirus = new Cards('green_virus', '/assets/cards/green_virus.png', 4, 'virus', 'green');
+let rainbowVirus = new Cards('rainbow_virus', '/assets/cards/rainbow_virus.png', 1, 'virus', 'rainbow');
 
-let contagion = new Cards('contagion', '/assets/contagion.png', 2, 'treatment', 'purple');
-let thief = new Cards('thief', '/assets/thief.png', 3, 'treatment', 'purple');
-let transplant = new Cards('transplant', '/assets/transplant.png', 3, 'treatment', 'purple');
-let glove = new Cards('glove', '/assets/glove.png', 1, 'treatment', 'purple');
-let medicalError = new Cards('medical_error', '/assets/medical_error.png', 1, 'treatment', 'purple');
+let contagion = new Cards('contagion', '/assets/cards/contagion.png', 2, 'treatment', 'purple');
+let thief = new Cards('thief', '/assets/cards/thief.png', 3, 'treatment', 'purple');
+let transplant = new Cards('transplant', '/assets/cards/transplant.png', 3, 'treatment', 'purple');
+let glove = new Cards('glove', '/assets/cards/glove.png', 1, 'treatment', 'purple');
+let medicalError = new Cards('medical_error', '/assets/cards/medical_error.png', 1, 'treatment', 'purple');
 
 
 
